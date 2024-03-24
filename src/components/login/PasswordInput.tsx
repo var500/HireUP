dd250fa3b5be121be342f0517819254693ad1bab
var500 <varun5chodha@gmail.com>
2024-03-24 19:16:43 +0530

    Add: SignUp page ui

--- src/components/login/PasswordInput.tsx
+++ src/components/login/PasswordInput.tsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Eye, EyeOff } from "lucide-react";
 
 export default function PasswordInput({
   visible,
-  onChange,
+  onVisibilityChange,
 }: {
   visible: boolean;
-  onChange: () => void;
+  onVisibilityChange: () => void;
 }) {
+  const [password, setPassword] = useState("");
   return (
-    <div className="grid w-full items-center gap-1.5">
-      <Label htmlFor="password">Password</Label>
-      <div className="relative flex flex-row w-full items-center">
-        <Input
-          type={`${visible ? "text" : "password"}`}
-          id="password"
-          placeholder="Password"
-        />
-        <div
-          className={`absolute right-2 ${
-            visible
-              ? "text-red-600 hover:text-red-700"
-              : "text-gray-600 hover:text-black"
-          } hover:cursor-pointer 
+    <div className="relative flex flex-row w-full items-center">
+      <Input
+        type={`${visible ? "text" : "password"}`}
+        id="password"
+        value={password}
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div
+        className={`absolute right-2 ${
+          visible
+            ? "text-red-600 hover:text-red-700"
+            : "text-gray-600 hover:text-black"
+        } hover:cursor-pointer 
                   `}
-          onClick={onChange}
-        >
-          {visible ? <EyeOff size={20} /> : <Eye size={20} />}
-        </div>
+        onClick={onVisibilityChange}
+      >
+        {visible ? <EyeOff size={20} /> : <Eye size={20} />}
       </div>
     </div>
   );
